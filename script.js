81662758ea892f65c56cd4f7b7b90eee6785f338
Marco Chico <mc0905091@otc.edu>
2021-11-12 16:03:01 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// Marco Chico
+// 11/11/2021
+// Web Dev 2
+
+//Story helped made using https://www.plot-generator.org.uk/?i=p0e0mme
+
+const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
+
+const log = console.log;
+
+// Title
+log(chalk.red.bold.underline('\t\t\tZoe Humble and the Three Wicked Lizards'));
+log();
+
+//Story
+log(chalk.bgBlackBright(chalk.white(`Once upon a time there was a ruddy girl called Zoe Humble.`)));
+log(chalk.red('She was on the way to see her friends Garth Greenway, when she decided to take a short cut through Gurglebridge.'));
+log(chalk.green('It wasn`t long before Zoe got lost.'))
+log(chalk.yellow('She looked around, but all she could see were trees.'));
+log(chalk.blue('Nervously, she felt into her bag for her favourite toy, Blankey, but Blankey was nowhere to be found!'));
+log(chalk.magenta('Zoe began to panic.'));
+log(chalk.cyan('She felt sure she had packed Blankey.'));
+log(chalk.white('To make matters worse, she was starting to feel hungry.'));
+log(chalk.blackBright('Unexpectedly, she saw a wicked lizard dressed in a blue pinafore disappearing into the trees.'));
+log(chalk.bgBlackBright(chalk.white(`"How odd!" thought Zoe.`)));
+log(chalk.red('For the want of anything better to do, she decided to follow the peculiarly dressed lizard.'));
+log(chalk.green('Perhaps it could tell him the way out of the forest.'));
+log(chalk.yellow('Eventually, Zoe reached a clearing.'));
+log(chalk.blue('She found herself surrounded by houses made from different sorts of food.'));
+log(chalk.magenta('There was a house made from swedes, a house made from chocolates, a house made from humbugs and a house made from chips.'));
+log(chalk.cyan('Zoe could feel her tummy rumbling.'));
+log(chalk.white('Looking at the houses did nothing to ease her hunger.'));
+log(chalk.bgBlackBright(chalk.white(`"Hello!" she called. "Is anybody there?" Nobody replied.`)));
+log(chalk.red('Zoe looked at the roof on the closest house and wondered if it would be rude to eat somebody else`s chimney.'));
+log(chalk.green('"Obviously it would be impolite to eat a whole house, but perhaps it would be considered acceptable to nibble the odd fixture or lick the odd fitting, in a time of need.'));
+log();
+//Author
+chalkAnimation.neon('\t\t\t\tBy Marco Chico')
